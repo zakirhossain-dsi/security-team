21ef1e2be4aaf24acab26b4770a19c449c7d1129
Zakir <zakir.mbstu.ict07@gmail.com>
2026-01-18 15:39:00 +0800

    Added tfe_outputs

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
-data "terraform_remote_state" "network" {
-  backend = "remote"
-  config = {
-    organization = "rbts"
-    workspaces = {
-      name = "network-team"
-    }
-  }
+data "tfe_outputs" "network" {
+  organization = "rbts"
+  workspace = "network-team"
 }
 
 output "network_public_ips" {
-  value = data.terraform_remote_state.network.outputs.public_ips
+  value = data.tfe_outputs.network.values.public_ips
   description = "List of public IPs fetched from the Network workspace"
 }
